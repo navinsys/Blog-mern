ced725895ea5100f061ac2dffb3932ba72d8a55b
Navinkumar Singh <singhnavinkumar493@gmail.com>
2025-01-04 18:19:09 +0530

    Created Blog section into pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import NoPage from './pages/NoPage'
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+import SingleBlog from "./pages/SingleBlog";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>}/> 
-            <Route path="*" element={<NoPage/>}/> 
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:blogId" element={<SingleBlog />} />
+        <Route path="/nopage" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Here from '../components/Here'
 import Blogs from '../components/Blogs';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar/>
       <Here/>
       <Blogs/>
+      <Footer/>
     </>
   )
 }
