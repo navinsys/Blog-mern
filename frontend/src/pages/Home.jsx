2ded84b5bec9392806bdee3b8e91783083f2e6a1
Navinkumar Singh <singhnavinkumar493@gmail.com>
2025-01-04 15:51:50 +0530

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Here from '../components/Here'
+import Blogs from '../components/Blogs';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Here/>
+      <Blogs/>
+    </>
+  )
+}
+
+export default Home;
---
+++ frontend/src/pages/NoPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoPage = () => {
+  return (
+    <div>
+      No Page 404
+    </div>
+  )
+}
+
+export default NoPage;
